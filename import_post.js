a1a1e39627b4463543697ec414012afd3bf7eb0b
takashimamorino <does1026@gmail.com>
2020-11-13 14:55:51 +0900

    feat: fetch users

--- import_post.js
+++ import_post.js
@@ -1,11 +1,14 @@
+const axios = require('axios')
 const {
   query
 } = require('graphqurl')
 
-query({
-  query: 'mutation { insert_post(objects: {body: "body01", id: 11, title: "titile01", userId: 1}) { returning {body title id userId}} }',
-  endpoint: 'http://localhost:8080/v1/graphql',
-  headers: {
-    'x-access-key': 'postgrespassword',
-  }
-}).catch(err => console.log(err))
+axios.get('https://jsonplaceholder.typicode.com/posts/').then(res => console.log(res.data))
+
+// query({
+//   query: 'mutation { insert_post(objects: {body: "body01", id: 11, title: "titile01", userId: 1}) { returning {body title id userId}} }',
+//   endpoint: 'http://localhost:8080/v1/graphql',
+//   headers: {
+//     'x-access-key': 'postgrespassword',
+//   }
+// }).catch(err => console.log(err))
